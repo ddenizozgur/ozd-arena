e91c281cb18179748d4d05775f2e582879a4a225
ddenizozgur <ozerdeniz63246@gmail.com>
2026-04-29 03:37:23 +0300

    elo

--- arena/pre.hpp
+++ arena/pre.hpp
@@ -117,3 +117,17 @@
 #if !defined(IS_OS_LINUX)
 #define IS_OS_LINUX 0
 #endif
+
+/*
+ *
+ */
+
+#if IS_COMPILER_MSVC
+#if !defined(_MSVC_LANG) || _MSVC_LANG < 201703L
+#error Requires a C++17 compiler
+#endif
+#else
+#if !defined(__cplusplus) || __cplusplus < 201703L
+#error Requires a C++17 compiler
+#endif
+#endif
